5a01fb226d28307b9966f191e9ae770ca0d5de42
Camilo Cubillos <juan.cubillos@sofka.com.co>
2021-05-25 18:03:52 -0500

    Fixed api source

--- lib/data_source/api/my_api_source.dart
+++ lib/data_source/api/my_api_source.dart
@@ -1,29 +1,22 @@
 import 'package:http/http.dart' as http;
 import 'package:injectable/injectable.dart';
+import 'package:rick_morty/app/application.dart';
 import 'package:rick_morty/connectivity/connectivity.dart';
+import 'package:rick_morty/connectivity/connectivity_adapter.dart';
 
 import 'api_source.dart';
 
 @singleton
+@injectable
 class MyApiSource with ApiSource {
-  final String _baseUrl;
-  final http.Client _client;
-  final Connectivity _connectivity;
-
-  MyApiSource(
-    this._baseUrl,
-    this._client,
-    this._connectivity,
-  );
+  @override
+  String get baseUrl => Application().appSettings.baseUrl;
 
   @override
-  String get baseUrl => _baseUrl;
+  http.Client get client => http.Client();
 
   @override
-  http.Client get client => _client;
-
-  @override
-  Connectivity get connectivity => _connectivity;
+  Connectivity get connectivity => ConnectivityAdapter();
 
   @override
   Map<String, String> getHeaders(Map<String, String> headers) {
--- lib/di/injection.config.dart
+++ lib/di/injection.config.dart
@@ -5,34 +5,28 @@
 // **************************************************************************
 
 import 'package:data/character_repository.dart' as _i3;
-import 'package:domain/character_use_case.dart' as _i7;
+import 'package:domain/character_use_case.dart' as _i6;
 import 'package:get_it/get_it.dart' as _i1;
-import 'package:http/http.dart' as http;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:rick_morty/app/application.dart';
-import 'package:rick_morty/connectivity/connectivity_adapter.dart';
-import 'package:rick_morty/data_source/api/my_api_source.dart';
 
-import '../blocs/characters_bloc.dart' as _i6;
-import '../data_source/api/character_api_source.dart' as _i4;
-import '../data_source/data_base/character_db_source.dart' as _i5;
+import '../blocs/characters_bloc.dart' as _i5;
+import '../data_source/api/character_api_source.dart' as _i7;
+import '../data_source/api/my_api_source.dart' as _i8;
+import '../data_source/data_base/character_db_source.dart' as _i4;
 import '../data_source/data_base/config/app_database.dart'
-    as _i8; // ignore_for_file: unnecessary_lambdas
+    as _i9; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
 _i1.GetIt $initGetIt(_i1.GetIt get,
     {String environment, _i2.EnvironmentFilter environmentFilter}) {
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
+  gh.factory<_i3.CharacterDbSource>(() => _i4.CharacterApiSourceAdapter());
+  gh.factory<_i5.CharactersBloc>(
+      () => _i5.CharactersBloc(get<_i6.CharacterUseCase>()));
   gh.factory<_i3.CharacterApiSource>(
-      () => _i4.CharacterApiSourceAdapter(MyApiSource(
-            Application().appSettings.baseUrl,
-            http.Client(),
-            ConnectivityAdapter(),
-          )));
-  gh.factory<_i3.CharacterDbSource>(() => _i5.CharacterApiSourceAdapter());
-  gh.factory<_i6.CharactersBloc>(
-      () => _i6.CharactersBloc(get<_i7.CharacterUseCase>()));
-  gh.singleton<_i8.AppDatabase>(_i8.AppDatabase());
+      () => _i7.CharacterApiSourceAdapter(get<_i8.MyApiSource>()));
+  gh.singleton<_i9.AppDatabase>(_i9.AppDatabase());
+  gh.singleton<_i8.MyApiSource>(_i8.MyApiSource());
   return get;
 }
